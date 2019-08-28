0585832d88ddb7e09e3cdfcb804a057121e8ccc0
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-28 15:58:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,4 +28,4 @@ def using_concat (my_favorite_things, all_my_favs)
   my_favorite_things.concat (all_my_favs)
 end
 
-def using_insert (array, element)
+def using_insert (list_of_programming_languages, )
