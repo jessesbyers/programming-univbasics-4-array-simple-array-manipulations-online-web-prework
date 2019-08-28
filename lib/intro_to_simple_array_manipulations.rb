afc6a3467733074b2e8c93142c056cf2c2eaf628
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-28 15:33:36 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push (array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  colors_in_the_rainbow.push ("violet")
+end
+
+def using_unshift (array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green"]
+  colors_in_the_rainbow.unshift ("Staten Island")
+end
+
+def using_pop (array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
