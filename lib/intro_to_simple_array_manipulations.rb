4fcc0a44c160afef6ada2062436e6533cfa94e87
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-28 16:01:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,5 +29,5 @@ def using_concat (my_favorite_things, all_my_favs)
 end
 
 def using_insert (list_of_programming_languages, "Python")
-  list_of_programming_languages.insert ("Python")
+  list_of_programming_languages.insert (4, "Python")
 end
